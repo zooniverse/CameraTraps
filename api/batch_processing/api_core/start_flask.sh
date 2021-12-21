3bb81d566b1c8e29ec8d3d56eb6c444c6a8567e9
Campbell Allen <campbell.allen@gmail.com>
2021-12-21 17:16:50 +0000

    use correct string cmp

--- api/batch_processing/api_core/start_flask.sh
+++ api/batch_processing/api_core/start_flask.sh
@@ -2,7 +2,7 @@
 
 LISTEN_PORT=${LISTEN_PORT:=80}
 
-if [ "$FLASK_ENV" == "production" ]; then
+if [ "$FLASK_ENV" = "production" ]; then
   echo Starting production server
   exec gunicorn -b 0:$LISTEN_PORT -w 1 --threads 4 -t 60 --access-logfile - --capture-output server:app
 else
